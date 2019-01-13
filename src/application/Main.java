49145df60270e9c779d37dc4e751dc81ecf717cb
Carlos Salvador <carlos1992.ces@gmail.com>
2019-01-13 07:39:27 -0200

    Projeto criado

---
+++ src/application/Main.java
@@ -0,0 +1,31 @@
+package application;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+		
+		int num = 0, resultado, i;
+		
+		System.out.println("----------Programa para calcular tabuada!!!---------");
+		
+		try {
+	    	System.out.print("Digite um número para calcular a tabuada: ");
+		    num = sc.nextInt();
+		
+		   for(i=1; i<=10; i++) {
+			  resultado = num * i;
+			    System.out.printf("%d X %d = %d%n", num, i, resultado);
+			}
+		}
+		catch(InputMismatchException err) {
+				System.out.println("Digite apenas números inteiros!");
+		        System.out.println("----------Fim do Programa!---------");
+		}
+		
+		sc.close();
+	}
+}
